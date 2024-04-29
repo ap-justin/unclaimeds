fa38494363e8c45973dbe967919ae054df6f0dee
ap-justin <89639563+ap-justin@users.noreply.github.com>
2024-04-29 17:08:07 +0800

    use marshaller

--- pub78-processor/helpers.ts
+++ pub78-processor/helpers.ts
@@ -1,3 +1,4 @@
+import { marshall } from "@aws-sdk/util-dynamodb";
 import { lastItemEIN } from "./setup";
 import type { Algolia, EndowData, V3Endowment } from "./types";
 
@@ -26,89 +27,29 @@ export const algoliaIndexFormat: Formatter<Algolia.EndowRecord> = (
   };
 };
 
-type RemoveOptional<T> = {
-  [K in keyof T as undefined extends T[K] ? never : K]: T[K];
-};
-type Data = RemoveOptional<V3Endowment.Endow.DBRecord> &
-  //include as state and city is available in pub78
-  Required<Pick<V3Endowment.Endow.DBRecord, "street_address">>;
-type Key = keyof Data;
-type V<K extends Key> = Data[K];
-type DT<K extends Key> = V<K> extends boolean
-  ? "BOOL"
-  : V<K> extends string
-    ? "S"
-    : V<K> extends number
-      ? "N"
-      : V<K> extends unknown[]
-        ? "L"
-        : V<K> extends Record<string, unknown>
-          ? "M"
-          : "";
-
-type PrimitiveFormat<T> = T extends number
-  ? `${T}`
-  : T extends string[]
-    ? { S: string }[]
-    : T extends number[]
-      ? { N: `${number}` }[]
-      : T;
-
-type DynamoDBJson = {
-  [K in Key]-?: V<K> extends undefined
-    ? undefined
-    : Record<DT<K>, PrimitiveFormat<V3Endowment.Endow.DBRecord[K]>>;
-};
-
-export const dynamoDBFormat: Formatter<DynamoDBJson> = (id: number, endow) => {
+export const dynamoDBFormat: Formatter<Record<string, any>> = (
+  id: number,
+  endow
+) => {
   const name = `${endow.env === "staging" ? "test-" : ""}${endow.name}`;
-  return {
-    PK: {
-      S: `Endow#${id}`,
-    },
-    SK: {
-      S: "production",
-    },
-    id: {
-      N: `${id}`,
-    },
-    env: {
-      S: "production",
-    },
-    registration_number: {
-      S: endow.ein,
-    },
-    name: {
-      S: name,
-    },
-    active_in_countries: {
-      L: [{ S: "United States" }],
-    },
-    claimed: {
-      BOOL: false,
-    },
-    kyc_donors_only: {
-      BOOL: false,
-    },
-    fiscal_sponsored: {
-      BOOL: false,
-    },
-    endow_designation: {
-      S: "Charity",
-    },
-    hq_country: {
-      S: "United States",
-    },
-    sdgs: {
-      L: [],
-    },
-    social_media_urls: {
-      M: {},
-    },
-    street_address: {
-      S: [endow.city, endow.state].filter(Boolean).join(", "),
-    },
+  const unmarshalled: V3Endowment.Endow.DBRecord = {
+    PK: `Endow#${id}`,
+    SK: endow.env,
+    id: id,
+    env: endow.env,
+    registration_number: endow.ein,
+    name,
+    active_in_countries: ["United States"],
+    claimed: false,
+    kyc_donors_only: false,
+    fiscal_sponsored: false,
+    endow_designation: "Charity",
+    hq_country: "United States",
+    sdgs: [],
+    social_media_urls: {},
+    street_address: [endow.city, endow.state].filter(Boolean).join(", "),
   };
+  return marshall(unmarshalled);
 };
 
 export const writer =
