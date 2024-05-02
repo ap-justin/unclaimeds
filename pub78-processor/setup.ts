55a36c6b77e435c0f111e9e1e7482bdda3de9d5d
ap-justin <89639563+ap-justin@users.noreply.github.com>
2024-05-02 17:28:26 +0800

    new branch

--- pub78-processor/setup.ts
+++ pub78-processor/setup.ts
@@ -3,13 +3,13 @@ import path from "node:path";
 import readline from "node:readline";
 import zlib from "node:zlib";
 import type { Environment } from "./types";
-const inputFilePath = path.join(__dirname, "pub78-test.txt"); //NOTE: update lastItem EIN
+const inputFilePath = path.join(__dirname, "pub78.txt"); //NOTE: update lastItem EIN
 const dynamoDbFilePath = path.join(__dirname, "dynamodbjson.txt.gz");
 const algoliaIndexFilePath = path.join(__dirname, "algolia.json");
 const errorFilePath = path.join(__dirname, "errors.txt");
 /** used to determine if put trailing comma */
-export const lastItemEIN = "002237333";
-export const env: Environment = "staging";
+export const lastItemEIN = "999999062";
+export const env: Environment = "production";
 export const startingId = 200; //make sure to query endowments_v2 per stage
 
 export const inputStream = fs.createReadStream(inputFilePath, {
